449e9814cfa54d56c073a49f4d09b1574211d24e
Kiara Atkins <atkinsk98@gmail.com>
2019-09-09 10:16:39 -0400

    users can be added to the database

--- src/main/java/co/grandcircus/FinalProject/controller/LoginController.java
+++ src/main/java/co/grandcircus/FinalProject/controller/LoginController.java
@@ -3,6 +3,7 @@ package co.grandcircus.FinalProject.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import co.grandcircus.FinalProject.jpaEntity.User;
@@ -11,34 +12,32 @@ import co.grandcircus.FinalProject.repository.UserRepository;
 @Controller
 public class LoginController {
 	
-	
-	User user;
-	
 	@Autowired
 	UserRepository repo;
 	
+	//Making a bunch of changes here, change it back it if doesn't work
 	@RequestMapping("log-in")
-	public ModelAndView login(User user) {
-		ModelAndView mv;
-		//this checks to see if the user data we pull will exist in the database
-		
-		if (repo.findByEmail(user.getEmail()).size() == 0) {
-			mv = new ModelAndView("page","testing", user);
-		}
-		else {
-			//if they arent this goes to a registration page
-			mv = new ModelAndView("register", "testing", user);
-		}
-		mv = new ModelAndView("register", "testing", user);
-		return mv;
-		
+	public ModelAndView login() {
+		return new ModelAndView("log-in");
+	}
+	
+	@RequestMapping("login")
+	public ModelAndView getUser(@RequestParam("email") String email, @RequestParam("password") String password) {
+		User user = new User(email, password);
+			if (repo.findByEmail(email).size() == 0) {
+				repo.save(user);
+				return new ModelAndView("register-success", "user", user);
+			}
+			else {
+				return new ModelAndView("returning-user", "user", user);
+			}
 	}
 	
 	@RequestMapping("register")
 	public ModelAndView register(User user) {
 		ModelAndView mv;
 		if (repo.findByEmail(user.getEmail()).size() == 0) {
-			mv = new ModelAndView("log-in");
+			mv = new ModelAndView("page", "user", user);
 		}
 		else {
 			repo.save(user);
---
+++ src/main/webapp/WEB-INF/views/page.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+IF YOU GET HERE, YOUR COE THINKS THE USER DOESNT YET EXIST
+</body>
+</html>
