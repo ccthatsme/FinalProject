90ddad4e4c4f5cc1290f411d07dd13a1c1ce470f
Chris <cciric44@gmail.com>
2019-09-12 12:01:23 -0400

    added checkbox functionality so we can search for recipes

--- src/main/java/co/grandcircus/FinalProject/controller/MealController.java
+++ src/main/java/co/grandcircus/FinalProject/controller/MealController.java
@@ -1,3 +1,4 @@
+
 package co.grandcircus.FinalProject.controller;
 
 import java.util.ArrayList;
@@ -15,44 +16,81 @@ import co.grandcircus.FinalProject.repository.PantryRepository;
 
 @Controller
 public class MealController {
-	
+
 	Food food;
 	Food foodTwo;
-	
+
 	@Autowired
 	PantryRepository pRepo;
-	
+
 	@Autowired
 	FoodRepository fRepo;
-	
+
 	@RequestMapping("meal-page")
 	public ModelAndView mealHome() {
 		List<Food> list = fRepo.findAll();
-		
+
 		return new ModelAndView("meal", "list", list);
-		
+
 	}
-	
+
+//	@RequestMapping("edit-meal")
+//	public ModelAndView addToMeal(@RequestParam("id") int id, @RequestParam("check") boolean check) {
+//		ModelAndView mv = new ModelAndView();
+//		List<Food> caloriesList = new ArrayList<Food>();
+//		if(check) {
+//			foodTwo = fRepo.getOne(id);
+//			caloriesList.add(foodTwo);
+//		}
+//		
+//		
+//		food = fRepo.getOne(id);
+//		int calories = food.getCalories();
+//		List<Food> list = fRepo.findAll();
+//		double sum = 0;
+//		for ( Food food : list) {
+//			sum = food.getCalories() + sum;
+//			
+//		}
+//		mv = new ModelAndView("page", "test", caloriesList);
+//		return mv;
+//	}
+//	
+
 	@RequestMapping("edit-meal")
-	public ModelAndView addToMeal(@RequestParam("id") int id, @RequestParam("check") boolean check) {
-		ModelAndView mv = new ModelAndView();
-		List<Food> caloriesList = new ArrayList<Food>();
-		if(check) {
-			foodTwo = fRepo.getOne(id);
-			caloriesList.add(foodTwo);
-		}
-		
-		
-		food = fRepo.getOne(id);
-		int calories = food.getCalories();
-		List<Food> list = fRepo.findAll();
-		double sum = 0;
-		for ( Food food : list) {
-			sum = food.getCalories() + sum;
-			
-		}
-		mv = new ModelAndView("page", "test", caloriesList);
+	public ModelAndView addToMeal(@RequestParam("check") String check) {
+		System.out.println(check);
+		String[] ingredients = check.split(",");
+
+		for (int i = 0; i < ingredients.length; i++) {
+			if (i == 0) {
+				System.out.print(ingredients[i]);
+			} else if (i != 0) {
+				System.out.print(",+");
+				System.out.print(ingredients[i]);
+			}
+			/*
+			 * // } else if (i== ingredients.length-1) { // System.out.print(",+"); //
+			 * System.out.print(ingredients[i]); // }
+			 */ }
+		ModelAndView mv = new ModelAndView("page");
+//		List<Food> caloriesList = new ArrayList<Food>();
+//		if(check) {
+//			foodTwo = fRepo.getOne(id);
+//			caloriesList.add(foodTwo);
+//		}
+//		
+//		
+//		food = fRepo.getOne(id);
+//		int calories = food.getCalories();
+//		List<Food> list = fRepo.findAll();
+//		double sum = 0;
+//		for ( Food food : list) {
+//			sum = food.getCalories() + sum;
+//			
+//		}
+		// mv = new ModelAndView("page", "test", caloriesList);
 		return mv;
 	}
-	
+
 }
